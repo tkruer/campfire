8d2fa3232620f4c01ec53a58e40e6b37dc5a820e
Tyler K <tylerkruer1@gmail.com>
2023-12-13 07:54:05 -0700

    trying this

--- site/test.py
+++ site/test.py
@@ -7,12 +7,12 @@ def custom_do_GET(self):
     self.end_headers()
     self.wfile.write("<h1>Welcome to the Home Page</h1>".encode())
 
-# Assign the new method to SimpleHTTPRequestHandler
+# Assign the new metahod to SimpleHTTPRequestHandler
 SimpleHTTPRequestHandler.do_GET = custom_do_GET
 
 if __name__ == "__main__":
     # Server setup
     port = 8000
-    httpd = HTTPServer(('localhost', port), SimpleHTTPRequestHandler)
+    httpd = HTTPServer(("0.0.0.0", port), SimpleHTTPRequestHandler)
     print(f"Serving at port {port}")
     httpd.serve_forever()
